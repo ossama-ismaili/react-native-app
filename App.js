ec4fd251767f9beacb53fe1627c67d5488bbece1
Ossama Ismaili <oussamaismaili06@gmail.com>
2020-03-15 21:47:10 +0100

    ReactNative Components

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import React ,{ Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Main from './components/MainComponent';
 
-class App extends Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Main />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default App;
